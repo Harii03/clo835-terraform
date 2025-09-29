67bbc8adaaff8472616c014023d8ba2e59b09795
Harii03 <86625634+Harii03@users.noreply.github.com>
2025-09-28 23:01:58 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,37 +22,6 @@ resource "aws_ecr_repository" "mysql" {
   image_tag_mutability = "MUTABLE"
 }
 
-# --- IAM for EC2 to pull from ECR (+ optional SSM) ---
-data "aws_iam_policy_document" "ec2_trust" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "ec2_role" {
-  name               = "LabRole"
-  assume_role_policy = data.aws_iam_policy_document.ec2_trust.json
-}
-
-resource "aws_iam_role_policy_attachment" "ecr_readonly" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-resource "aws_iam_role_policy_attachment" "ssm_core" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "clo835-ec2-profile"
-  role = aws_iam_role.ec2_role.name
-}
-
 # --- Security Group (SSH + app ports) ---
 resource "aws_security_group" "web_sg" {
   name        = "clo835-web-sg"
