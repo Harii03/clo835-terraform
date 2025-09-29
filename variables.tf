7c0e1a214716dd4a73c5a463800183f4f70ae25f
Harii03 <86625634+Harii03@users.noreply.github.com>
2025-09-28 21:24:31 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,2 +1,11 @@
-variable "region"       { type = string, default = "us-east-1" }  # change later if needed
-variable "key_pair_name"{ type = string, description = "EC2 key pair name for SSH" }
+cat > variables.tf <<'EOF'
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "key_pair_name" {
+  type        = string
+  description = "EC2 key pair name for SSH"
+}
+EOF
