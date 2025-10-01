370075bd4020fd4f31e0ab5a5f10a8770610a706
Harii03 <86625634+Harii03@users.noreply.github.com>
2025-10-01 02:47:23 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,2 @@
-variable "region" {
-  description = "AWS region to deploy into"
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "key_name" {
-  description = "Existing EC2 key pair name in the target region"
-  type        = string
-}
+variable "region"   { type = string  default = "us-east-1" }
+variable "key_name" { type = string  description = "Existing EC2 key pair name" }
