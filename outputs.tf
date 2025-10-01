98cb95ac975ce496229720fc99114584f65ff250
Harii03 <86625634+Harii03@users.noreply.github.com>
2025-10-01 02:54:58 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,15 @@
-output "ec2_public_ip"  { value = aws_instance.ec2.public_ip }
-output "ec2_public_dns" { value = aws_instance.ec2.public_dns }
-output "ecr_web_repo"   { value = aws_ecr_repository.webapp.repository_url }
-output "ecr_mysql_repo" { value = aws_ecr_repository.mysql.repository_url }
+output "ec2_public_ip" {
+  value = aws_instance.ec2.public_ip
+}
+
+output "ec2_public_dns" {
+  value = aws_instance.ec2.public_dns
+}
+
+output "ecr_web_repo" {
+  value = aws_ecr_repository.webapp.repository_url
+}
+
+output "ecr_mysql_repo" {
+  value = aws_ecr_repository.mysql.repository_url
+}
