46c4eb3f02fe60379e4e36db3da4b099bdda0f4a
Harii03 <86625634+Harii03@users.noreply.github.com>
2025-10-01 02:23:50 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "key_name" {
-  description = "Existing EC2 key pair name in the target region"
+variable "key_pair_name" {
+  description = "Existing EC2 key pair name"
   type        = string
 }
